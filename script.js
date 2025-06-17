13ddc664dca7644849c13b464418dbd875e0de63
devbhumicodes <bhumi.kasikaran@gmail.com>
2025-06-17 08:29:44 +0100

    Update script.js
    Moved slideshow to side of text, java adjusted accordingly

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ let slideIndex = 0;
 const slides = document.querySelectorAll('.slide');
 
 function showSlides() {
-  slides.forEach((slide, i) => {
-    slide.classList.remove('active');
-  });
+  slides.forEach(slide => slide.classList.remove('active'));
   slideIndex = (slideIndex + 1) % slides.length;
   slides[slideIndex].classList.add('active');
 }
 
-slides[0].classList.add('active');
-setInterval(showSlides, 3000);
+slides[0].classList.add('active'); //first slide
+setInterval(showSlides, 3000); //3 second interval
