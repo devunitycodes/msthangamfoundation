314041aab310bcfcd5ed586fdaf047704374fa0f
devbhumicodes <bhumi.kasikaran@gmail.com>
2025-06-17 19:00:16 +0100

    Update script.js
    added the navbar hamburger function

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let slideIndex = 0;
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll('.slides');
 
 function showSlides() {
-  slides.forEach(slide => slide.classList.remove('active'));
+  slides.forEach(slide => slide.style.display = 'none');
   slideIndex = (slideIndex + 1) % slides.length;
-  slides[slideIndex].classList.add('active');
+  slides[slideIndex].style.display = 'block';
 }
 
-slides[0].classList.add('active'); //first slide
-setInterval(showSlides, 3000); //3 second interval
+slides[0].style.display = 'block'; // Show first image
+setInterval(showSlides, 3000); // Rotate every 3 sec
+
+
+function myFunction() {
+  var x = document.querySelector(".topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
